39b5ef4d31984d20fd787e176f9214e7cf05ccd7
Aniket Yadav <117531261+aniket0657@users.noreply.github.com>
2025-10-02 21:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const toggles = document.querySelectorAll(".toggle");
+
+toggles.forEach(toggle => {
+  toggle.addEventListener("change", () => {
+    const checkedToggles = [...toggles].filter(t => t.checked);
+
+    // Only allow max 2 toggles
+    if (checkedToggles.length > 2) {
+      // Uncheck the first one
+      checkedToggles[0].checked = false;
+    }
+  });
+});
